671a789fe5aa7117ac2e288af4e404e605150d1a
Danesh Lachman <daneshlachman@gmail.com>
2021-11-23 17:29:27 +0100

    initial commit with first draft of a working ecli-obtainer

---
+++ src/ecli_obtainer.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pdb
+
+DRIVER_PATH = r'chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://uitspraken.rechtspraak.nl/#zoekverfijn/zt[0][zt]=winkeldiefstal&zt[0]'
+           '[fi]=&zt[0][ft]=Alle+velden&idx=1&so=Relevance')
+
+list_of_ECLI = []
+
+
+# parse ecli-code from the string in the webelement
+def ecli_parser(ECLI_string):
+    parsed_string = ''
+    for character in ECLI_string:
+        if character == ' ':
+            break
+        else:
+            parsed_string += character
+    return parsed_string
+
+
+number_of_results = driver.find_element(By.XPATH,'//*[@id="content"]/div/div[2]/div[1]/h2/span/span').text
+
+i = 1
+counter = 1
+
+# loop through all of the cases, and append the ECLI-codes to a list
+for x in range(int(number_of_results)):
+    if counter > 50:
+        break
+    if counter % 10 == 0:
+        driver.find_element(By.XPATH, '//*[@id="laadmeer"]').click()
+    search_result_element = driver.find_element(By.XPATH,'//*[@id="zoekresultaatregion"]/div/div[{0}]/div[1]/h3/a'.format(i))
+    list_of_ECLI.append(ecli_parser(search_result_element.text))
+    driver.implicitly_wait(3)
+    i += 1
+    counter += 1
+
+# read ECLI codes, and write them to ECLI_list.txt
+text_file = open("./ECLI_list.txt", "w")
+for element in list_of_ECLI:
+    text_file.write(element + "\n")
+text_file.close()
+
+driver.quit()
---
+++ src/main.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+
+# DRIVER_PATH = r'chromedriver.exe'
+# driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+url = 'https://uitspraken.rechtspraak.nl/inziendocument?id={0}'
+
+ecli_file = open('ECLI_list.txt', 'r')
+ecli_lines = ecli_file.readlines()
+
+
+
+# driver.get(url.format(ecli_lines[0].strip()))
+
+# print(url.format(ecli_lines[0].strip()))
+
+count = 0
+for ecli in ecli_lines:
+    count += 1
+    response = requests.get(url.format(ecli_lines[count].strip()))
+    content = response.content
+    parser = BeautifulSoup(content, 'html.parser')
+    body = parser.body
+    producten = body.find(class_="uitspraak")
+    if producten == None:
+        Exception(ValueError)
+    print(producten.prettify())
+
+
+
+
+# response = requests.get(url.format(ecli_lines[0].strip()))
+# content = response.content
+# parser = BeautifulSoup(content, 'html.parser')
+# body = parser.body
+# producten = body.find(class_="uitspraak")
+# print(producten.prettify())
