79459d2423ebee6630528c294898ab6aa39ac067
Danesh Lachman <daneshlachman@gmail.com>
2022-01-07 14:39:09 +0100

    better preprocessing for tenlasteleggingen

--- src/tenlasteleggingen_obtainer.py
+++ src/tenlasteleggingen_obtainer.py
@@ -18,8 +18,17 @@ tenlastelegging_list = ['inleiding en tenlastelegging', 'vrijspraak primair ten 
                         'tekst gewijzigde tenlastelegging', 'tenlastelegging', 'tenlasteleggingen',
                         'de tenlastelegging', 'inhoud van de tenlastelegging', 'tekst tenlasteleggingen',
                         'de inhoud van de tenlastelegging', 'de tenlasteleggingen',
-                        'vrijspraak van het primair ten laste gelegde', 'tenlasteleggingen']
+                        'vrijspraak van het primair ten laste gelegde', 'tenlasteleggingen', 'De tenlastelegging.',]
 
+list_of_not_valuable_string = 'Aan de verdachte is ten laste gelegd dat: De verdachte is ten laste gelegd dat: ' \
+                              'De tenlastelegging is als bijlage aan dit vonnis gehecht. De gewijzigde tenlastelegging' \
+                              ' is als bijlage aan dit vonnis gehecht. Aan verdachte is ten laste gelegd dat: Aan verdachte' \
+                              ' is, na wijziging van de tenlastelegging, ten laste gelegd dat: Aan verdachte is tenlastegelegd dat: De ' \
+                              'verdenking komt er, kort en feitelijk weergegeven, op neer dat de verdachte:'
+
+bijlage_list = ['bijlage', 'Bijlage']
+
+list_of_not_valuable_string = list_of_not_valuable_string.lower()
 
 # read all possible headers in a string
 with open('../txt_files/header_list.txt') as f:
@@ -31,19 +40,26 @@ list_of_uitspraken = []
 # get all tenlasteleggingen
 count = 0
 for ecli in ecli_lines:
+    # ecli = 'ECLI:NL:RBGEL:2020:6212'
+    # if count > 1:
+    #     break
     header_text = ''
     # retrieve uitspraak
     response = requests.get(url.format(ecli_lines[count].strip()))
-    # response = requests.get(url.format('ECLI:NL:RBNNE:2013:1780'))
+    # response = requests.get(url.format('ECLI:NL:RBGEL:2020:6212'))
     content = response.content
     parser = BeautifulSoup(content, 'html.parser')
     body = parser.body
     uitspraak = body.find(class_="uitspraak")
+
+    # do some preprocessing to recognize the headers effectively, and append tenlastelegging text
+    # to a string until a header is encountered
     try:
         spraaktest = [line for line in uitspraak.text.split('\n') if line.strip() != '']
         in_header = False
         for line in spraaktest:
-            line = line.strip().lstrip("1234567890 .")
+            line = line.strip().lstrip("1234567890 .\t")
+            # pdb.set_trace()
             if line == '':
                 continue
             if line in all_headers_string and in_header:
@@ -52,9 +68,16 @@ for ecli in ecli_lines:
                 in_header = True
                 continue
             if in_header:
-                header_text += line
-        # print(ecli)
-        # print(header_text)
+                if line.lower() not in list_of_not_valuable_string:
+                    header_text += line
+
+        # if tenlastelegging text refers to the bijlage, then the text
+        # should be retrieved somewhere else or partially ignored
+        if 'bijlage' in header_text or 'Bijlage' in header_text:
+            header_text = ''
+
+        print(ecli)
+        print(header_text)
         list_of_uitspraken.append([ecli.strip(), header_text])
     except ValueError:
         print("something wrong with uitspraak")
